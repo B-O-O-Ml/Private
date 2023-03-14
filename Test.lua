dc6d879aa0588caab1b969fe6f738357747b4219
GMT + 7 <101545758+B-O-O-Ml@users.noreply.github.com>
2023-03-14 11:20:31 +0700

    Create Test.lua

---
+++ Test.lua
@@ -0,0 +1,14 @@
+local Whitelist = {
+    Discord = "ALL",
+    Keys = {
+        ["B-O-O-M#4134-OWNER-Private"] = { Expiration = os.time{year=2023, month=3, day=2, hour=7, min=0, sec=0}, IsActive = true },
+        ["BxB-xyz-FGDO-MIZG-Premium"] = { Expiration = os.time{year=2023, month=12, day=31, hour=23, min=59, sec=59}, IsActive = true },
+        ["BxB-xyz-BBSH-AHEJ-Premium"] = { Expiration = os.time{year=2024, month=6, day=30, hour=23, min=59, sec=59}, IsActive = true },
+    },
+    Hwid = {
+        "3F27C118-5600-4F51-968A-994212D20A64",
+        "FF0272E9-3286-4B88-AB02-C722A4B1618EF",
+        "FF0272E9-3286-4B88-AB02-C722A4B1618",
+    },
+}
+return Whitelist
